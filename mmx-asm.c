0673314e7a558309b7f1e8ff30eb63952e51d5ec
toni <toni@zweihorn>
2015-09-04 17:24:41 +0200

    make mmx-asm.c compatible with clang.

--- mmx-asm.c
+++ mmx-asm.c
@@ -1,5 +1,6 @@
 // What does COMISS/UCOMISS do?
 
+#define _BSD_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -31,56 +32,60 @@ void mmx_using_asm(void) {
 	}
 }
 
-typedef short int v8ssi __attribute__ ((vector_size (16)));
+typedef short int v4ssi __attribute__ ((vector_size (8)));
 
 void mmx_using_intrinsics1(void) {
-	v8ssi a = {0,1,2,3,4,5,6,7};
-	v8ssi b = {0,1,2,3,-4,-5,12,14};
-	v8ssi c;
-	for (int i=0; i<8; i++) {
+	v4ssi a = {0,1,4,5};
+	v4ssi b = {0,1,-4,10};
+	v4ssi c;
+	for (int i=0; i<4; i++) {
 		printf("a[%i]:%i\tb[%i]:%i\tc[%i]:%i\n", i, a[i], i, b[i], i, c[i]);
 	}
 	printf("c = a <= b;\n");
 	c = a <= b;
-	for (int i=0; i<8; i++) {
+	for (int i=0; i<4; i++) {
 		printf("a[%i]:%i\tb[%i]:%i\tc[%i]:%i\n", i, a[i], i, b[i], i, c[i]);
 	}
 }
 
-typedef int v4si __attribute__ ((vector_size (16)));
+typedef int v2si __attribute__ ((vector_size (8)));
 
+/*
+// TODO: find out what the equivalent of __builtin_shufle is for clang.
 void mmx_using_intrinsics2(void) {
-	v4si a = {random(),random(),random(),random()};
-	v4si b = {random(),random(),random(),random()};
-	v4si ign = {(random()%2)==0?0:-1,(random()%2)==0?0:-1,(random()%2)==0?0:-1,(random()%2)==0?0:-1};
-	v4si mask = {random()%8, random()%8, random()%8, random()%8};
-	v4si c;
-	for (int i=0; i<4; i++) {
+	v2si a = {random(),random()};
+	v2si b = {random(),random()};
+	v2si ign = {(random()%2)==0?0:-1,(random()%2)==0?0:-1};
+	v2si mask = {random()%8, random()%8};
+	v2si c;
+	for (int i=0; i<2; i++) {
 		printf("a[%i]:%i\tb[%i]:%i\tign[%i]:%i\tc[%i]:%i\n", i, a[i], i, b[i], i, ign[i], i, c[i]);
 	}
 	printf("c = (a & ign) | (b & ~ign);\n");
 	c = (a & ign) | (b & ~ign);
-	for (int i=0; i<4; i++) {
+	for (int i=0; i<2; i++) {
 		printf("a[%i]:%i\tb[%i]:%i\tign[%i]:%i\tc[%i]:%i\n", i, a[i], i, b[i], i, ign[i], i, c[i]);
 	}
 	printf("c = __builtin_shuffle(a, b, mask);\n");
 	c = __builtin_shuffle(a, b, mask);
-	for (int i=0; i<4; i++) {
+	for (int i=0; i<2; i++) {
 		printf("a[%i]:%i\tb[%i]:%i\tmask[%i]:%i\tc[%i]:%i\n", i, a[i], i, b[i], i, mask[i], i, c[i]);
 	}
 }
+*/
 
+int randint() {
+	return random() * ((random()%2)==0?1:-1);
+}
 void mmx_using_intrinsics3(void) {
-	int randint() {
-		return random() * ((random()%2)==0?1:-1);
-	}
-	v4si a = {randint(),randint(),randint(),randint()};
-	for (int i=0; i<4; i++) {
+	v2si a = {randint(),randint()};
+	v2si b = {16,16};
+	for (int i=0; i<2; i++) {
 		printf("a[%i]:%i\n", i, a[i]);
 	}
 	printf("a = a >> 16;\n");
-	a = a >> 16;
-	for (int i=0; i<4; i++) {
+	a = a >> b;
+	for (int i=0; i<2; i++) {
 		printf("a[%i]:%i\n", i, a[i]);
 	}
 }
