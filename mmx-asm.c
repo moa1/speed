90166a2efa06b6e8e140614e0d940de3d4b1bbc5
toni <toni@pc1400>
2015-09-05 20:25:06 +0200

    more examples of mmx and sse.

--- mmx-asm.c
+++ mmx-asm.c
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int randint() {
+	return random() * ((random()%2)==0?1:-1);
+}
+
 void mmx_using_asm(void) {
 	short int i0[4]={0,2,3,4};
 	//short int i1[4]={-1,3,3,0};
@@ -32,11 +36,40 @@ void mmx_using_asm(void) {
 	}
 }
 
+void sse_using_asm(void) {
+//	int i0[4]={ 1001002003,2001002003,501002003,251002003};
+//	int i1[4]={-1001002003,2091002003,500000000,251002003};
+	int i0[4]={randint(),randint(),randint(),randint()};
+	int i1[4]={randint(),randint(),randint(),randint()};
+	
+	for (int i=0; i<4; i++) {
+		printf("i0[%i]:%i\ti1[%i]:%i\n", i, i0[i], i, i1[i]);
+	}
+
+	printf("----\n");
+	__asm__(
+		"movq %0,%%xmm0\n"
+		"movq %1,%%xmm1\n"
+		"pmaxsw %%xmm0, %%xmm1\n"
+		"movq %%xmm0,%0\n"
+		"movq %%xmm1,%1\n"
+		: "=m"(i0), "=m"(i1)
+		: "m"(i0), "m"(i1)
+		: "0", "1"
+	);
+
+	for (int i=0; i<4; i++) {
+		printf("i0[%i]:%i\ti1[%i]:%i\n", i, i0[i], i, i1[i]);
+	}
+}
+
+typedef int v4si __attribute__((vector_size(16)));
+
 typedef short int v4ssi __attribute__ ((vector_size (8)));
 
 void mmx_using_intrinsics1(void) {
-	v4ssi a = {0,1,4,5};
-	v4ssi b = {0,1,-4,10};
+	v4ssi a = {randint(),randint(),randint(),randint()};
+	v4ssi b = {randint(),randint(),randint(),randint()};
 	v4ssi c;
 	for (int i=0; i<4; i++) {
 		printf("a[%i]:%i\tb[%i]:%i\tc[%i]:%i\n", i, a[i], i, b[i], i, c[i]);
@@ -46,6 +79,12 @@ void mmx_using_intrinsics1(void) {
 	for (int i=0; i<4; i++) {
 		printf("a[%i]:%i\tb[%i]:%i\tc[%i]:%i\n", i, a[i], i, b[i], i, c[i]);
 	}
+	printf("v4si d = {c[0],c[1],c[2],c[3]};\n");
+	v4si d = {c[0],c[1],c[2],c[3]};
+	for (int i=0; i<4; i++) {
+		printf("c[%i]:%i d[%i]:%i\n", i, c[i], i, d[i]);
+	}
+
 }
 
 typedef int v2si __attribute__ ((vector_size (8)));
@@ -74,9 +113,6 @@ void mmx_using_intrinsics2(void) {
 }
 */
 
-int randint() {
-	return random() * ((random()%2)==0?1:-1);
-}
 void mmx_using_intrinsics3(void) {
 	v2si a = {randint(),randint()};
 	v2si b = {16,16};
@@ -92,7 +128,11 @@ void mmx_using_intrinsics3(void) {
 
 int main(void) {
 
-	mmx_using_intrinsics3();
+	//mmx_using_asm();
+	//sse_using_asm();
+	mmx_using_intrinsics1();
+	//mmx_using_intrinsics2();
+	//mmx_using_intrinsics3();
 	
 	return 0;
 }
